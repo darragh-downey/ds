ec352e37b502d099a9f45cb98979daf1cafe015b
Darragh Downey <darragh.downey90@gmail.com>
2021-04-28 17:27:33 +1000

    added root level gitignore

--- rs/target/rls/debug/deps/rs-67cd12bf442f9de9.d
+++
@@ -1,7 +0,0 @@
-/home/ddowney/Workspace/github.com/DS/rs/target/rls/debug/deps/rs-67cd12bf442f9de9.rmeta: src/lib.rs src/ds.rs src/alg.rs
-
-/home/ddowney/Workspace/github.com/DS/rs/target/rls/debug/deps/rs-67cd12bf442f9de9.d: src/lib.rs src/ds.rs src/alg.rs
-
-src/lib.rs:
-src/ds.rs:
-src/alg.rs:
--- rs/target/rls/debug/deps/rs-d4981c0e481ef558.d
+++
@@ -1,7 +0,0 @@
-/home/ddowney/Workspace/github.com/DS/rs/target/rls/debug/deps/rs-d4981c0e481ef558.rmeta: src/lib.rs src/ds.rs src/alg.rs
-
-/home/ddowney/Workspace/github.com/DS/rs/target/rls/debug/deps/rs-d4981c0e481ef558.d: src/lib.rs src/ds.rs src/alg.rs
-
-src/lib.rs:
-src/ds.rs:
-src/alg.rs:
